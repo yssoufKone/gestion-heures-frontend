669e38dc0f22dfaa22c7d6703b57e070beef3685
yssoufkone <koney1759@gmail.com>
2026-04-05 11:48:03 +0000

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://gestheure-api.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
